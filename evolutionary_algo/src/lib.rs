5b5083648af202fa9a5fd281517ce7dfb5ba6cc9
Christophe Genevey-Metat <genevey.christophe@gmail.com>
2023-05-10 21:38:47 +0200

    first function success load into the main

--- evolutionary_algo/src/lib.rs
+++ evolutionary_algo/src/lib.rs
@@ -2,7 +2,16 @@
 mod individual;
 pub use individual::Individual;
 
-pub fn create_population() -> Vec<Individual> {
-    let individuals:Vec<Individual> = Vec::new();
+pub fn create_population(nb_population : u32) -> Vec<Individual> {
+    let mut individuals:Vec<Individual> = Vec::new();
+
+    for _ in 0..nb_population {
+        let mut gen: Vec<u32> = Vec::new();
+        gen.push(5);
+        gen.push(7);
+
+        let ind = Individual::new(gen);
+        individuals.push(ind);
+    }
     individuals
 }
--- evolutionary_algo/src/main.rs
+++ evolutionary_algo/src/main.rs
@@ -1,4 +1,8 @@
+use evolutionary_algo::Individual;
+use evolutionary_algo::create_population;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the evolutionary algorithm code !");
+    let population:Vec<Individual> = create_population( 5 );
+    println!("Population size: {:?}", population.len());
 }
